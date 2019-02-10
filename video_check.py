6b96195199e55852d64969bc335068dbe6be32e5
Mortuza Hossain <mortuzahossain1997@gmail.com>
2019-02-09 19:35:33 -1000

    Final

--- video_check.py
+++ video_check.py
@@ -2,6 +2,9 @@
 import numpy as np
 from cv2 import *
 cap = VideoCapture(0)
+import pygame
+pygame.mixer.init()
+
 
 faceDetactor = CascadeClassifier('haarcascade_frontalface_default.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -12,15 +15,19 @@ while(True):
     # Capture frame-by-frame
     ret, im = cap.read()
     # Display the resulting frame
-    
+
     gray = cvtColor(im,COLOR_BGR2GRAY)
     faces = faceDetactor.detectMultiScale(gray,1.3,5)
     for x,y,w,h in faces:
 	    rectangle(im,(x,y),(x+w,y+h),(0,0,255),2)
 	    ids,conf = recognizer.predict(gray[y:y+h,x:x+w])
 	    print(ids)
-	    if ids == 1: ids = "Sajjad"
-	    elif ids == 2: ids = "Mortuza"
+	    if ids == 1:
+		ids = "Sajjad"
+		pygame.mixer.music.load("sajjad.mp3")
+	    elif ids == 2:
+		ids = "Mortuza"
+		pygame.mixer.music.load("sajjad.mp3")
 	    putText(im,str(ids), (x,y-10), FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
     imshow('frame',im)
     if waitKey(1) & 0xFF == ord('q'):

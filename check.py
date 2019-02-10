be8e1d5d39ace30fdf35942a401c33787eb8bd11
Mortuza Hossain <mortuzahossain1997@gmail.com>
2019-02-09 19:47:07 -1000

    With Car Detection

--- check.py
+++ check.py
@@ -1,4 +1,3 @@
-import time
 import numpy as np
 from cv2 import *
 cap = VideoCapture(0)
@@ -8,27 +7,45 @@ pygame.mixer.init()
 faceDetactor = CascadeClassifier('haarcascade_frontalface_default.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("recognizerModel.yml")
+car_cascade = cv2.CascadeClassifier('cars.xml')
+
+def yeal(id):
+	if id == 1: pygame.mixer.music.load("sajjad.mp3")
+	elif id == 2: pygame.mixer.music.load("sajjad.mp3")
+	elif id == 100: pygame.mixer.music.load("car.mp3")
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:
+		continue
+
 
 while(True):	
 	ret, im = cap.read()
 	gray = cvtColor(im,COLOR_BGR2GRAY)
-	faces = faceDetactor.detectMultiScale(gray,1.3,5)
 	
+	faces = faceDetactor.detectMultiScale(gray,1.3,5)
+	cars = car_cascade.detectMultiScale(gray, 1.1, 1)
+	
+	# For Car Detection
+	ncars = 0
+	for (x, y, w, h) in cars:
+		cv2.rectangle(im, (x,y), (x+w,y+h), (0,0,0), 2)
+		ncars = ncars + 1
+	
+	print(ncars)
+	
+	# For Face Detection
 	for x,y,w,h in faces:
 		rectangle(im,(x,y),(x+w,y+h),(0,0,255),2)
 		ids,conf = recognizer.predict(gray[y:y+h,x:x+w])
-		print(ids)
+		
+		#print(ids)
+		
 		if ids == 1:
 			ids = "Sajjad"
-			pygame.mixer.music.load("sajjad.mp3")
 		elif ids == 2:
 			ids = "Mortuza"
-			pygame.mixer.music.load("mortuza.mp3")
-		
+		yeal(ids)	
 		putText(im,str(ids), (x,y-10), FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
-		pygame.mixer.music.play()
-		while pygame.mixer.music.get_busy() == True:
-			continue
 		
 	imshow('frame',im)
 	
